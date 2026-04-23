b185fe625acacd3a63994d14bc341175ad8f2f46
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-04-23 08:36:39 +0300

    feat: enable lingui/format-po

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,4 +1,5 @@
 import type { LinguiConfig } from "@lingui/conf";
+import { formatter } from "@lingui/format-po";
 
 const config: LinguiConfig = {
   locales: [
@@ -13,7 +14,7 @@ const config: LinguiConfig = {
     },
   ],
   sourceLocale: "en",
-  format: "po",
+  format: formatter({ lineNumbers: false }),
   fallbackLocales: {
     default: "en",
   },
